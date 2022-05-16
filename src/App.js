08b51e4fc08cb3a6fb6f0e08634abdd6235ab1be
guillermo.leal <guillermo01.galp@gmail.com>
2022-05-15 19:03:07 -0500

    chore: create and config routes, assets and cdns

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
-import logo from './logo.png';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useFetch } from './hooks/useFetch';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Search } from './components/Search';
+import { Home } from './views/Home/Home';
+import { ListMeals } from './views/ListMeals/ListMeals';
+import { MealDetails } from './views/MealDetails/MealDetails';
+import { BtnFavourites } from './components/BtnFavourites';
+import { Favourites } from './views/Favourites/Favourites';
+import { Error404 } from './views/Error/Error404';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to Arkangel Meals</h1>
-      </header>
-    </div>
-  );
+	const [search, setSearch] = useState('');
+
+	const handleSearch = (e) => {
+		setSearch(e.target.value);
+	};
+
+	const handleResetSearch = () => setSearch('');
+
+	const { data } = useFetch(`/search.php?f=${search}`, 'GET');
+
+	return (
+		<BrowserRouter>
+			<Header />
+			<BtnFavourites />
+			<div className="d-flex justify-center">
+				<h2 className="title">The most delicious meals you can find!</h2>
+			</div>
+			<div className="d-flex justify-center">
+				<Search
+					placeholder="Search Meal..."
+					onChange={handleSearch}
+					value={search}
+					items={data.meals || []}
+					reset={handleResetSearch}
+				/>
+			</div>
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="category">
+						<Route index element={<Error404 />} />
+						<Route path=":categoryId" element={<ListMeals />} />
+					</Route>
+					<Route path="meal">
+						<Route index element={<Error404 />} />
+						<Route path=":mealId" element={<MealDetails />} />
+					</Route>
+					<Route path="/favorites" element={<Favourites />} />
+					<Route path="*" element={<Error404 />} />
+				</Routes>
+			</main>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
-export default App;
+export { App };
