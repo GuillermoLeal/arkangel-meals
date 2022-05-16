1f2a63f5e6b5fd394c8c2c624ae8b242bd90231a
guillermo.leal <guillermo01.galp@gmail.com>
2022-05-15 21:00:13 -0500

    chore: config app to pwa

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<div>
+	<React.StrictMode>
 		<App />
-	</div>
+	</React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
